5d0f14347a3ccc919f9fe5387ca279a9b15e60c2
Chloedarcy <chloe_darcy@icloud.com>
2023-06-29 12:05:39 -0400

    fix add button, create delete and checkboxes

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,55 @@
-<script setup>
-import {ref} from "vue";
-import buttoncomp from "./components/buttoncomp.vue";
 
-const task = ref(68);
+<script setup>
+import { ref } from "vue";
 
 const msg = 'Todo App';
-const text = ref('Text');
+const text = ref('');
+const checked = ref(false);
+const tasks = ref([])
 
-function retrieveTask(){
-  console.log(text)
-  
+function addTask() {
+  tasks.value.push(text.value)
+  text.value = " "
 }
-
+function deleteTask(){
+  tasks.value.pop()
+}
 </script>
 
 <style>
 .title {
   font-size: 35px;
 }
-</style>
+.deleteButton{
+  font-size: 10px;
 
+}
+
+</style>
 <template>
-   <h1 class="font-bold title" >
-    {{ msg }}
-  </h1>
-  <div class ="flex mt-[10px]" >
-    <input v-model="text" placeholder="Task here">
-    <p>Message is: {{ text }}</p>
+  <h1 class="font-bold title">{{ msg }}</h1>
+  <div class="flex mt-[10px]">
+    <input v-model="text" type="input" placeholder="Task here">
   </div>
- 
+
   <div class="border border-gray-300 rounded-lg p-1 inline-block">
     <button
-    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-    @click="retrieveTask"
-    
-    >Add</button>
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      @click="addTask">Add Task</button>
   </div>
+  <!--<div>{{ transferredText }} <input type="checkbox" id="checkbox" v-model="checked"></div>
+  -->
 
   <div>
-    <input type="checkbox" id="checkbox" v-model="checked">
+    <ul>
+      <li v-for="task in tasks" :key="task">{{ task }} 
+        <label v-if="task != ' '">
+          <input type="checkbox" id="checkbox" v-model="task.checked">
+          <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-1 rounded deleteButton"
+          @click="deleteTask()">Delete</button>
+        </label>
+    </li>
+    </ul>
   </div>
 
 </template>
-
-
