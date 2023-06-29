4f8b3da68f3cc689a9c04c0da54aa1da1d781861
Chloedarcy <chloe_darcy@icloud.com>
2023-06-29 01:48:25 -0400

    added add button and text-box to receive user text

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,46 @@
 <script setup>
 import {ref} from "vue";
-import clickButton from "./components/buttoncomp.vue";
+import buttoncomp from "./components/buttoncomp.vue";
 
-const bobAge = ref(68);
+const task = ref(68);
 
 const msg = 'Todo App';
+const text = ref('Text');
+
+function retrieveTask(){
+  console.log(text)
+  
+}
+
 </script>
 
+<style>
+.title {
+  font-size: 35px;
+}
+</style>
+
 <template>
-  <h1 style="text-align: center;">
+   <h1 class="font-bold title" >
     {{ msg }}
   </h1>
   <div class ="flex mt-[10px]" >
-    <clickButton @click="bobAge++"  :name="''" :age="bobAge">This is some text</clickButton>
+    <input v-model="text" placeholder="Task here">
+    <p>Message is: {{ text }}</p>
   </div>
+ 
+  <div class="border border-gray-300 rounded-lg p-1 inline-block">
+    <button
+    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    @click="retrieveTask"
+    
+    >Add</button>
+  </div>
+
+  <div>
+    <input type="checkbox" id="checkbox" v-model="checked">
+  </div>
+
 </template>
+
 
